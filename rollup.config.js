a1320e6a13ff6e7a49688b5139d64e31b8a17386
hcpty hcpty <hcpty@outlook.com>
2024-09-21 02:55:09 +0800

    update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import terser from "@rollup/plugin-terser";
 
 export default {
-  plugins: [
-    process.env.MODE == "release" ? terser() : null,
-  ],
+  plugins: [process.env.MODE == "release" ? terser() : null],
   output: {
     format: "iife",
   },
